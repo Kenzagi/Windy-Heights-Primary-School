62066e30e0d5ceac4eb597166f0f02d857ef3f6f
Khayelihle Gumede <sphamandlajunior31@gmail.com>
2024-04-18 23:06:40 +0100

    Updating Website
    - Added 2 new pages (Gallery & About).
    - Added functionality on both pages.
    - Updated project resources.

--- script.js
+++ script.js
@@ -14,67 +14,23 @@ apply.addEventListener('click', (event) => {
 })
 
 
-// CALENDAR CODE;
+// GALLERY CODE;
 
-let currentMonth = document.querySelector(".current-month");
-let calendarDays = document.querySelector(".calendar-days");
-let today = new Date();
-let date = new Date();
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
 
-
-currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-today.setHours(0,0,0,0);
-renderCalendar();
-
-function renderCalendar(){
-    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
-    const totalMonthDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
-    const startWeekDay = new Date(date.getFullYear(),date.getMonth(),0).getDay();
-
-    calendarDays.innerHTML = "";
-
-    let totalCalendarDay = 6 * 7;
-    for (let i = 0; i < totalCalendarDay; i++) {
-        let day = i-startWeekDay;
-
-        if(i <= startWeekDay){
-            // adding previous month days
-            calendarDays.innerHTML += `${prevLastDay-i}`;
-        }else if(i <= startWeekDay+totalMonthDay){
-            // adding this month days
-            date.setDate(day);
-            date.setHours(0,0,0,0);
-
-            let dayClass = date.getTime()===today.getTime() ? 'current-day' : 'month-day';
-            calendarDays.innerHTML += `${day}`;
-        }else{
-            // adding next month days
-            calendarDays.innerHTML += `${day-totalMonthDay}`;
-        }
-    }
+for (var i = 1; i <= 105; i++) {
+	var thumb = document.createElement("img");
+	thumb.src = "images/Gallery/" + i + ".jpg";
+	thumb.alt = "Image " + i;
+	thumb.classList.add("thumb");
+	imageThumbs.appendChild(thumb);
+	
+	thumb.addEventListener(
+  "click", function() {
+	currentImage.src = this.src;
+	currentImage.alt = this.alt;
+  }
+);
 }
 
-document.querySelectorAll(".month-btn").forEach(function (element) {
-	element.addEventListener("click", function () {
-		date = new Date(currentMonth.textContent);
-        date.setMonth(date.getMonth() + (element.classList.contains("prev") ? -1 : 1));
-		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-		renderCalendar();
-	});
-});
-
-document.querySelectorAll(".btn").forEach(function (element) {
-	element.addEventListener("click", function () {
-        let btnClass = element.classList;
-        date = new Date(currentMonth.textContent);
-        if(btnClass.contains("today"))
-            date = new Date();
-        else if(btnClass.contains("prev-year"))
-            date = new Date(date.getFullYear()-1, 0, 1);
-        else
-            date = new Date(date.getFullYear()+1, 0, 1);
-
-		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-		renderCalendar();
-	});
-});
